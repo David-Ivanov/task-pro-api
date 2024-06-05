2a57c46909711f0974f997487990ff6bb19ff33d
David-Ivanov <111747295+David-Ivanov@users.noreply.github.com>
2024-06-05 19:49:03 +0300

    Update usersSchema.js

--- schemas/usersSchema.js
+++ schemas/usersSchema.js
@@ -5,6 +5,7 @@ export const createEditSchema = Joi.object({
     email: Joi.string().email().min(3),
     password: Joi.string().min(3).max(20),
     name: Joi.string().min(3).max(20),
+});
 
 export const createHelpSchema = Joi.object({
     email: Joi.string().email().min(3).required(),
