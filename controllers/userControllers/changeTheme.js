3fda2aff0cc4d9516f7dc987d919bc6662720f10
Ivanov David <ivanovdavid905@gmail.com>
2024-06-05 19:12:48 +0300

    fix bags in changeTheme.js

--- controllers/userControllers/changeTheme.js
+++ controllers/userControllers/changeTheme.js
@@ -1,5 +1,6 @@
-import HttpError from "../../helpers/HttpError";
-import User from "../../models/authModel";
+import HttpError from "../../helpers/HttpError.js";
+import User from "../../models/authModel.js";
+import jwt from "jsonwebtoken";
 
 const changeTheme = async (req, res) => {
     // get token
@@ -8,6 +9,12 @@ const changeTheme = async (req, res) => {
     const data = jwt.decode(token);
 
     const { theme } = req.body;
+
+    // validate
+    if (theme !== "light" && theme !== "dark" && theme !== "violet") {
+        return res.status(400).send({ message: HttpError(400).message });
+    }
+
     try {
         await User.findByIdAndUpdate(
             data.id,
