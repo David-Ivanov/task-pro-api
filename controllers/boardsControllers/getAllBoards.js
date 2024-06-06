7933dfb84cba48bb19174908fe19c126356a2b47
culplate <culplate@gmail.com>
2024-06-06 11:18:52 +0200

    added getAllBoards

--- controllers/boardsControllers/getAllBoards.js
+++ controllers/boardsControllers/getAllBoards.js
@@ -1,5 +1,12 @@
-const getAllBoards = () => {
+import Board from "../../models/boardsModel.js";
 
-}
+const getAllBoards = async (req, res, next) => {
+  try {
+    const boards = await Board.find({ owner: req.user._id });
+    return res.status(200).send(boards);
+  } catch (e) {
+    next(e);
+  }
+};
 
 export default getAllBoards;
