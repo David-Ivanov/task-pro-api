5b6bcce9f069146a0c6de0909b38bfa59834177c
Ivanov David <ivanovdavid905@gmail.com>
2024-06-07 12:25:23 +0300

    fix avatar and editProfile

--- controllers/userControllers/updateAvatar.js
+++ controllers/userControllers/updateAvatar.js
@@ -2,14 +2,24 @@ import Jimp from "jimp";
 import HttpError from "../../helpers/HttpError.js";
 import User from "../../models/authModel.js"
 import jwt from "jsonwebtoken";
-import fs from "node:fs/promises";
 import { v2 as cloudinary } from 'cloudinary';
+import fs from "fs/promises";
+import { createAvatarSchema } from "../../schemas/usersSchema.js";
 
 
 const updateAvatar = async (req, res) => {
     // errors are catching in authMiddleware.js
 
     try {
+        const { path, mimetype } = req.file;
+
+        // validate
+        const { error } = createAvatarSchema.validate(mimetype);
+
+        if (error) {
+            return res.status(400).send({ message: HttpError(400, "Wrong format").message });
+        }
+
         // get token
         const authorizationHeader = req.headers.authorization.split(" ");
         const token = authorizationHeader[1];
@@ -23,18 +33,17 @@ const updateAvatar = async (req, res) => {
             );
         }
 
-        const { path: tempPath } = req.file;
 
         // change size
-        const avatar = await Jimp.read(tempPath);
-        avatar.resize(250, 250).write(tempPath);
+        const avatar = await Jimp.read(path);
+        avatar.resize(250, 250).write(path);
 
-        const result = await cloudinary.uploader.upload(tempPath, {
+        const result = await cloudinary.uploader.upload(path, {
             asset_folder: "avatars",
             resource_type: "image"
         });
 
-        await fs.unlink(tempPath);
+        await fs.unlink(path);
 
         const user = await User.findByIdAndUpdate(
             data.id,
@@ -45,7 +54,7 @@ const updateAvatar = async (req, res) => {
             { new: true }
         );
 
-        res.status(200).send({ data: { avatarURL: user.avatarURL } });
+        return { avatarURL: user.avatarURL }
     } catch (error) {
         res.status(500).send({ message: HttpError(500).message });
     }
