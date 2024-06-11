ed14b19a833109b5e371988c39b7e5302a3c72b7
David-Ivanov <111747295+David-Ivanov@users.noreply.github.com>
2024-06-11 14:39:47 +0300

    Update getAllBoards.js

--- controllers/boardsControllers/getAllBoards.js
+++ controllers/boardsControllers/getAllBoards.js
@@ -4,7 +4,7 @@ import HttpError from "../../helpers/HttpError.js";
 const getAllBoards = async (req, res, next) => {
   try {
     const boards = await Board.find({ owner: req.user._id });
-    return res.status(200).send({ boards });
+    return res.status(200).send({ ...boards });
   } catch (e) {
     return res.status(500).send({ message: HttpError(500, e.message).message });
   }
